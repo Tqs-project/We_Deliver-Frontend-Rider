19be2259bc0caa82686b6b62e0ea0497df291b16
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-05-28 15:55:48 +0100

    178331413-Added List Of Orders

--- drinkup/lib/main.dart
+++ drinkup/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,105 +10,201 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Drink Up',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Main(title: 'Drink Up'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class Main extends StatefulWidget {
+  Main({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainState createState() => _MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MainState extends State<Main> {
+  final Color foregroundColor = Colors.grey[200];
+  int groupValue = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[OrderList(), CurrentOrder()],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+
+  Widget OrderList() {
+    return Container(
+      alignment: Alignment.topCenter,
+      margin: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Center(
+              child: GFListTile(
+                titleText: 'Orders',
+                subTitleText: 'Accept an Order',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Divider(
+              height: 40,
+              thickness: 5,
+            ),
+            ListView(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                GFRadioListTile(
+                  titleText: 'Arthur Shelby',
+                  subTitleText: 'By order of the peaky blinders',
+                  avatar: GFAvatar(
+                    backgroundImage: AssetImage('purchase-red.jpg'),
+                  ),
+                  size: 25,
+                  activeBorderColor: Colors.green,
+                  focusColor: Colors.green,
+                  type: GFRadioType.square,
+                  value: 0,
+                  groupValue: groupValue,
+                  onChanged: (value) {
+                    setState(() {
+                      groupValue = value;
+                    });
+                  },
+                  inactiveIcon: null,
+                ),
+                GFRadioListTile(
+                  titleText: 'Arthur Shelby',
+                  subTitleText: 'By order of the peaky blinders',
+                  avatar: GFAvatar(
+                    backgroundImage: AssetImage('purchase-red.jpg'),
+                  ),
+                  size: 25,
+                  activeBorderColor: Colors.green,
+                  focusColor: Colors.green,
+                  type: GFRadioType.square,
+                  value: 1,
+                  groupValue: groupValue,
+                  onChanged: (value) {
+                    setState(() {
+                      groupValue = value;
+                    });
+                  },
+                  inactiveIcon: null,
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget CurrentOrder() {
+    return Container(
+      alignment: Alignment.topCenter,
+      margin: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: GFListTile(
+                titleText: 'Orders',
+                subTitleText: 'Accept an Order',
+                icon: ElevatedButton.icon(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.add, size: 18),
+                  label: Text("Accept"),
+                ),
+              ),
+            ),
+            Divider(
+              height: 40,
+              thickness: 5,
+            ),
+            Center(
+                child: Container(
+              alignment: Alignment.topCenter,
+              margin: EdgeInsets.all(20),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Column(
+                    children: [
+                      Text(
+                        'Orders',
+                      ),
+                      Text('Orders')
+                    ],
+                  )),
+                  Expanded(
+                      child: Column(
+                    children: [
+                      Text(
+                        'Orders',
+                      ),
+                      Text('Orders')
+                    ],
+                  )),
+                ],
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
