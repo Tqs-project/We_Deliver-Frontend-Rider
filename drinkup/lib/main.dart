1826a0607cb5cf4f4c0ec9f11cb6a128292f007b
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-05-29 13:32:34 +0100

    178331413-Added Order details

--- drinkup/lib/main.dart
+++ drinkup/lib/main.dart
@@ -34,11 +34,15 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: Container(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(31, 29, 47, 1),
+      ),
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[OrderList(), CurrentOrder()],
       ),
-    );
+    ));
   }
 
   Widget OrderList() {
@@ -47,7 +51,7 @@ class _MainState extends State<Main> {
       margin: EdgeInsets.all(20),
       width: double.infinity,
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: Color.fromRGBO(111, 111, 164, 0.1),
         borderRadius: BorderRadius.circular(30),
         boxShadow: [
           BoxShadow(
@@ -69,8 +73,8 @@ class _MainState extends State<Main> {
               ),
             ),
             Divider(
-              height: 40,
-              thickness: 5,
+              height: 20,
+              thickness: 2,
             ),
             ListView(
               scrollDirection: Axis.vertical,
@@ -84,8 +88,8 @@ class _MainState extends State<Main> {
                     backgroundImage: AssetImage('purchase-red.jpg'),
                   ),
                   size: 25,
-                  activeBorderColor: Colors.green,
-                  focusColor: Colors.green,
+                  activeBorderColor: Colors.grey,
+                  radioColor: Colors.red[300],
                   type: GFRadioType.square,
                   value: 0,
                   groupValue: groupValue,
@@ -103,8 +107,8 @@ class _MainState extends State<Main> {
                     backgroundImage: AssetImage('purchase-red.jpg'),
                   ),
                   size: 25,
-                  activeBorderColor: Colors.green,
-                  focusColor: Colors.green,
+                  activeBorderColor: Colors.grey,
+                  radioColor: Colors.red[300],
                   type: GFRadioType.square,
                   value: 1,
                   groupValue: groupValue,
@@ -125,86 +129,135 @@ class _MainState extends State<Main> {
 
   Widget CurrentOrder() {
     return Container(
-      alignment: Alignment.topCenter,
-      margin: EdgeInsets.all(20),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: GFListTile(
-                titleText: 'Orders',
-                subTitleText: 'Accept an Order',
-                icon: ElevatedButton.icon(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  icon: Icon(Icons.add, size: 18),
-                  label: Text("Accept"),
-                ),
-              ),
+        alignment: Alignment.topCenter,
+        margin: EdgeInsets.all(20),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(111, 111, 164, 0.1),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 2),
             ),
-            Divider(
-              height: 40,
-              thickness: 5,
-            ),
-            Center(
-                child: Container(
-              alignment: Alignment.topCenter,
-              margin: EdgeInsets.all(20),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: Column(
-                    children: [
-                      Text(
-                        'Orders',
-                      ),
-                      Text('Orders')
-                    ],
-                  )),
-                  Expanded(
-                      child: Column(
-                    children: [
-                      Text(
-                        'Orders',
-                      ),
-                      Text('Orders')
-                    ],
-                  )),
-                ],
-              ),
-            ))
           ],
         ),
-      ),
-    );
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          GFListTile(
+            titleText: 'Current Order',
+            icon: ElevatedButton.icon(
+              onPressed: () {
+                // Respond to button press
+              },
+              icon: Icon(Icons.add, size: 18),
+              label: Text("Accept"),
+            ),
+          ),
+          Divider(
+            height: 20,
+            thickness: 2,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                    alignment: Alignment.topCenter,
+                    margin: EdgeInsets.all(20),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey[300],
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text("Order",
+                              style: new TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16)),
+                          Divider(
+                            height: 5,
+                            thickness: 5,
+                          ),
+                          ListView(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            padding: const EdgeInsets.all(8),
+                            children: <Widget>[
+                              GFListTile(
+                                titleText: 'Vinho do Porto',
+                                subTitleText: '1L',
+                                avatar: GFAvatar(
+                                  backgroundImage:
+                                      AssetImage('purchase-red.jpg'),
+                                ),
+                              ),
+                              GFListTile(
+                                titleText: 'Eristoff Vodka',
+                                subTitleText: '1.5L',
+                                avatar: GFAvatar(
+                                  backgroundImage:
+                                      AssetImage('purchase-red.jpg'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text("User Details",
+                        style: new TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                    Divider(
+                      height: 5,
+                      thickness: 5,
+                    ),
+                    ListView(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(8),
+                      children: <Widget>[
+                        GFListTile(
+                          titleText: 'Arthur Shelby',
+                          subTitleText: '4Km Away',
+                        ),
+                        GFListTile(
+                          subTitleText: 'Address: ---------',
+                        ),
+                        GFListTile(
+                          subTitleText: 'Phone Number: 913 514 255',
+                        ),
+                        Divider(
+                          height: 5,
+                          thickness: 5,
+                        ),
+                        Text(
+                          "Total: 31,4€",
+                          textAlign: TextAlign.right,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ))
+            ],
+          )
+        ]));
   }
 }
