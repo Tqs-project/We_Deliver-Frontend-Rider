617b45a1aa3643957804d0d7e71b402b8b91c985
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-05-31 16:43:50 +0100

    [#178331410] - Added Statistics

--- drinkup/lib/main.dart
+++ drinkup/lib/main.dart
@@ -1,113 +1,272 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Drink Up',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Main(title: 'Drink Up'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class Main extends StatefulWidget {
+  Main({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainState createState() => _MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MainState extends State<Main> {
+  final Color foregroundColor = Colors.grey[200];
+  String typeOfTransport = "CAR";
+  int groupValue = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Drink Up"),
         ),
+        body: Container(
+            height: (MediaQuery.of(context).size.height),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(31, 29, 47, 1),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[Profile(context), Statistics(context)],
+            )));
+  }
+
+  Widget Profile(BuildContext context) {
+    return Center(
+        child: Container(
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.all(10),
+      height: (MediaQuery.of(context).size.height) / 1.7,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(new Radius.circular(8)),
+        color: Color.fromRGBO(40, 40, 61, 0.8),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      child: ListView(
+        children: [
+          Hero(
+              tag: "Profile Picture",
+              child: Container(
+                child: Icon(
+                  Icons.person,
+                  size: 96,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 1.0,
+                  ),
+                  shape: BoxShape.circle,
+                  //image: DecorationImage(image: this.logo)
+                ),
+              )),
+          Hero(
+            tag: "Harry Potter",
+            child: Material(
+              type: MaterialType.transparency,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Center(
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    initialValue: "Harry Potter",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.only(top: 20), // add padding to adjust text
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(top: 15), // add padding to adjust icon
+                child: Icon(Icons.location_city, color: Colors.red),
+              ),
+            ),
+            initialValue: "Privet Drive",
+            style: TextStyle(color: Colors.white),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.only(top: 20), // add padding to adjust text
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(top: 15), // add padding to adjust icon
+                child: Icon(Icons.email, color: Colors.red),
+              ),
+            ),
+            initialValue: "hp@hogwarts.com",
+            style: TextStyle(color: Colors.white),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.only(top: 20), // add padding to adjust text
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(top: 15), // add padding to adjust icon
+                child: Icon(Icons.phone, color: Colors.red),
+              ),
+            ),
+            initialValue: "913 415 285",
+            style: TextStyle(color: Colors.white),
+          ),
+          Divider(
+            height: 10,
+          ),
+          Center(
+            child: ToggleSwitch(
+              initialLabelIndex: 1,
+              minWidth: 90,
+              cornerRadius: 20.0,
+              activeFgColor: Colors.white,
+              inactiveBgColor: Colors.grey,
+              inactiveFgColor: Colors.white,
+              labels: ['', '', ''],
+              icons: [
+                Icons.directions_car,
+                Icons.directions_bike,
+                Icons.directions_walk
+              ],
+              activeBgColors: [
+                Colors.red[400],
+                Colors.red[400],
+                Colors.red[400]
+              ],
+              onToggle: (index) {
+                if (index == 0)
+                  typeOfTransport = 'RIDER';
+                else
+                  typeOfTransport = 'CLIENT';
+              },
+            ),
+          ),
+          Center(
+              child: RaisedButton(
+            onPressed: () {},
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(80.0)),
+            padding: const EdgeInsets.all(0.0),
+            child: Ink(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [Colors.red, Colors.white]),
+                borderRadius: BorderRadius.all(Radius.circular(80.0)),
+              ),
+              child: Container(
+                width: (MediaQuery.of(context).size.width) / 3,
+                constraints: const BoxConstraints(
+                    minWidth: 8.0,
+                    minHeight: 36.0), // min sizes for Material buttons
+                alignment: Alignment.center,
+                child: const Text(
+                  'Save',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          )),
+        ],
+      ),
+    ));
+  }
+
+  Widget Statistics(BuildContext context) {
+    return Expanded(
+        child: Container(
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.all(10),
+      height: (MediaQuery.of(context).size.height) / 2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(new Radius.circular(8)),
+        color: Color.fromRGBO(40, 40, 61, 0.8),
+      ),
+      child: ListView(
+        children: [
+          Hero(
+            tag: "Statistics",
+            child: Material(
+              type: MaterialType.transparency,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Center(
+                  child: Text(
+                    "Statistics (Today)",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Divider(
+            height: 10,
+            color: Colors.red[400],
+            thickness: 2,
+          ),
+          Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Number Of Orders Accepted",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold)),
+                  Text("12", style: TextStyle(color: Colors.white))
+                ],
+              )),
+          Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Total Profit",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold)),
+                  Text("43€", style: TextStyle(color: Colors.white))
+                ],
+              )),
+          Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Travelled Distance",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold)),
+                  Text("19.3Km", style: TextStyle(color: Colors.white))
+                ],
+              )),
+          Divider(
+            height: 10,
+          ),
+        ],
+      ),
+    ));
   }
 }
