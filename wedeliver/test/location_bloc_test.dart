d11bb76df2f3043047e8f6078348ee17aed51786
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-06-21 21:13:14 +0100

    Added Network Permissions

--- wedeliver/test/location_bloc_test.dart
+++ wedeliver/test/location_bloc_test.dart
@@ -20,31 +20,6 @@ void main() {
   when(client.post(Uri.parse(BASE_URL + ('/api/riders/location')),
           headers: anyNamed('headers'), body: anyNamed('body')))
       .thenAnswer((_) async => http.Response('POSTED', 200));
-  /*
-  var url =
-      'https://maps.googleapis.com/maps/api/directions/json?origin=0.0,0.0&destination=CUSTOMER&waypoints=STORE&key=AIzaSyBPi7DD20WaxRNbaic5aVNwV3mbWCnioHk';
-
-  when(client.get(Uri.parse(url), headers: anyNamed('headers'))).thenAnswer(
-      (_) async => http.Response(
-          '{ "routes": [ {"legs":[{"distance":{"value":0} }], "overview_polyline":{"points":"1"}} ]}',
-          200));
-
-  test(
-      'test CalculateRoute ------------------------------------------------------',
-      () async {
-    var response = await locBloc.calculateRoute('STORE', 'CUSTOMER', client);
-    expect(response.statusCode, equals(200));
-    expect(
-        json.decode(response.body)['routes'][0]['overview_polyline']['points'],
-        equals('1'));
-  });
-
-  test(
-      'test getDistance ------------------------------------------------------',
-      () async {
-    var response = await locBloc.getDistance('STORE', 'CUSTOMER', client);
-    expect(response, equals(0));
-  });*/
 
   test(
       'test postLocation ------------------------------------------------------',
